3f93e9897f66d61f9cd52378a4d8a5994549d74d
Cedric Tremolosa <cedric.tremolosa@gmail.com>
2016-11-13 22:05:25 +0000

    Added comments

--- Coursework/src/neuralNetwork/BackPropagator.java
+++ Coursework/src/neuralNetwork/BackPropagator.java
@@ -3,9 +3,6 @@ package neuralNetwork;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 public class BackPropagator {
 
